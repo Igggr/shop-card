d5d8ba734e9f54870aa108e4264789b070ffc517
Igggr <uchetzapis@yahoo.com>
2018-12-07 17:24:56 +0300

    simple animation added

--- cart.js
+++ cart.js
@@ -2,34 +2,57 @@ let subTotal = 0;
 let tax = 0;
 let totalWithTax = 0;
 let productsToOder = [];
+let wagonImage = "images/wagon.png";
+let goalCoord = [0, 0];   //wagons will move here
 
 var productsList = 
-        [productInfo("The Ring", 999999, "invisibility + 1000", "one ring for", "ring.jpg"),
-        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", "ring.jpg"),
-        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", "ring.jpg"),
-        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", "ring.jpg"),
-        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", "ring.jpg"),
-        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", "ring.jpg"),
-        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", "ring.jpg"),
-        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", "ring.jpg"),
-        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", "ring.jpg"),
-        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", "ring.jpg"),
-        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", "ring.jpg"),
-        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", "ring.jpg"),
-        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", "ring.jpg"),            
-        ]
+        [productInfo("The Ring", 999999, "invisibility + 1000", "one ring for", generateId(), "ring.jpg"),
+        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", generateId(), "ring.jpg"),
+        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", generateId(), "ring.jpg"),
+        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", generateId(), "ring.jpg"),
+        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", generateId(), "ring.jpg"),
+        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", generateId(), "ring.jpg"),
+        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", generateId(), "ring.jpg"),
+        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", generateId(), "ring.jpg"),
+        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", generateId(), "ring.jpg"),
+        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", generateId(), "ring.jpg"),
+        productInfo("Silmaril", 77777, "beateful stones", "was created in first epoch", generateId(), "ring.jpg"),            
+        ];
 
-function productInfo(name_, price_, description_, full_descr_, image_){
-    return {name: name_, price: price_, description: description_, full_descr: full_descr_, image: image_};
+function productInfo(name_, price_, description_, full_descr_, id_, image_){
+    return {name: name_, price: price_, description: description_, full_descr: full_descr_, id: id_, image: image_};
 }
 
+function closure() {
+    let n = 0;
+    return () => n++;
+}
 
+function increasingNumbers() {
+    return closure(); 
+}
+
+function generateId() { 
+    return `productId#${increasingNumbers()}`; 
+}
+/*
+$(window).resize() {
+    updateGoalCoord(goalCoord);
+}
+*/
+
+function updateGoalCoord(goalCoord) {
+    goalCoord[0] = window.innerWidth;
+    goalCoord[1] = 50;
+}
 
 $(function() {
+    updateGoalCoord(goalCoord);
     let rw = $(".row").children().children(".row");   //TODO: rewrite more accuratly
     console.log(productsList);
     productsList.forEach(function(product){
-        rw.html( rw.html() + productDiv(product));        
+        rw.append(productDiv(product));
+        //rw.html( rw.html() + productDiv(product));        
     });
 
     var products = $(".col.col-xs-12.col-md-6.col-lg-4");
@@ -42,18 +65,32 @@ $(function() {
 });
 
 function productDiv(product) {
+    /*
+    let dv = $("div").attr("id", product.id);
+    console.log("div..");
+    console.log(dv);
+    let innDv = $("div").html(`<img src=images/${product.image}>`);
+    dv.append(innDv);
+    dv.append(`<h2>${product.name}</h2>`);
+    dv.append(`<h2>${product.description}</h2>`);
+    dv.append(`<h2>${product.price}</h2>`);
+    console.log(dv);
+    return dv;
+    */
     return `<div class="col col-xs-12 col-md-6 col-lg-4">
-               <div>
+               <div id ="${product.id}">
                   <img src=images/${product.image}>
                </div>
                <h2>${product.name}</h2>
                <p>${product.description}</p>
                <p>\$${product.price}</p>
-           </div>`
+            </div>`
+           
 }
 
 
 function animationThenAToCart() { 
+    $(this).prev().prev().prev().prev().css("background-color", "red").animate({left: "1000px", opacity:0.1}, 700);
     let prcTag = $(this).prev();
     let price = parseFloat(prcTag.text().slice(1));
     let product = prcTag.prev().text();
@@ -64,8 +101,24 @@ function animationThenAToCart() {
     //showNewShoppingCartForm(product);
 }
 
+function makeWagon () {
+    return $("<img/>", {src: 'images/wagon.png' , class: "wagon", "z-index": 2});
+}
+
+
 function wagonMove(price, product) { //TODO: wagon must move
+    let wagon = makeWagon();
+    $("body").append(wagon);
+    wagon.animate({left: "1000px", top: 0,opacity: 0.3, width: '200px' }, 700);
+    //wagon.moveTo(window.innerWidth, 0);
+    //$("body").append("<img src = 'images/wagon.png'>");
+    //.animate({left: "+=1000px"}, 7000);
+    replaceWagon();
     addToCard(price, product);
+}
+
+function replaceWagon() {
+    
 }
 
 function addToCard(price, product) {
@@ -81,7 +134,7 @@ function removeOldShoppingCartForm() {
 
 function showNewShoppingCartForm(product) {
    $(".cart").append("<b>your order:<b>");
-   let ind = -1;
+   let ind = -1;                                        //TODO: does the product already in shopping list?
    if (ind != -1) {
        console.log("alrready added, just increase ammount");
        productsToOder[product] += 1;
@@ -97,7 +150,8 @@ function showNewShoppingCartForm(product) {
    });
    showCosts();
 }
-   
+
+function findInAlreadyAdded(product) {}
                     
 function showProductInShoppingCard(item) {
     let product = "pr";
